977db20265bc3ce15734eea339ad158ae0d4ebfc
Mark Lester Bolotaolo <mlab817@gmail.com>
2021-12-08 21:26:23 +0800

    updated folder

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,17 +32,7 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'permissions' => [
-                'file' => [
-                    'public' => 0664,
-                    'private' => 0600,
-                ],
-                'dir' => [
-                    'public' => 0775,
-                    'private' => 0700,
-                ],
-            ],
+            'root' => storage_path('app'),
         ],
 
         'public' => [
